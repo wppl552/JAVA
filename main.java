314c3344bb85638ea6ac32d0c39027f898a4e811
wppl552 <115657787+wppl552@users.noreply.github.com>
2022-10-12 23:11:07 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.utils.*
+
+public main{
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        
+        int x = in.nextInt();
+        
+    }
+}
